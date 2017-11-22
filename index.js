208bf308065245e93e0716e8c0d8321e0589650c
Algirdas <mrinter5@gmail.com>
2017-11-22 21:35:07 +0000

    really works now

--- index.js
+++ index.js
@@ -7,18 +7,30 @@ const app = express();
 const getItem = (path, head = false) => new Promise((resolve, reject) => {
     const pathParts = path.split('');
 
-    let getPath;
-
-    if (pathParts.shift() === '/') {
-        getPath = pathParts.join('');
-    } else {
-        getPath = path;
+    if (pathParts[0] === '/') {
+        pathParts.shift();
     }
+    
+    if (pathParts[pathParts.length - 1] === '/') {
+        pathParts.pop();
+    }
+    
+    const getPath = pathParts.join('');
 
     s3[head ? 'headObject' : 'getObject']({
         Bucket: process.env.BUCKET,
         Key: getPath
-    }, (error, res) => error ? reject(error) : resolve(res));
+    }, (error, res) => {
+        if (error) {
+            return reject(error);
+        }
+        
+        if (res && !res.ContentLength) { // directory or empty file
+            return reject();
+        }
+        
+        resolve(res);
+    });
 });
 
 const s3 = new AWS.S3({
@@ -27,22 +39,18 @@ const s3 = new AWS.S3({
 
 app.get('*', async (req, res) => {
     let reqPath = decodeURIComponent(req.path);
-
+    
     try {
         const item = await getItem(reqPath);
-
         res.type(item.ContentType).end(item.Body);
     } catch (error) {
+        const indexPath = path.join(reqPath, 'index.html');
+        
         try {
-            const indexPath = path.join(reqPath, 'index.html');
-
-            if (await getItem(indexPath, true)) {
-                res.redirect(indexPath);
-            } else {
-                res.status(404).end();
-            }
-        } catch (indexError) {
-            res.status(400).end();
+            await getItem(indexPath, true); // Will fail if item is missing
+            res.redirect(indexPath);
+        } catch (error) {
+            res.status(404).end();
         }
     }
 });
